7398344e940b628826fa7fc2866249982e5e4e3c
MarioTheOne <m.a.prado89@outlook.com>
2022-02-08 15:10:50 +0100

    Update greedy.py
    -greedy_pivot renamed to generate_temporal_pivot_candidates
    -Added comments to the code
    -Added logic for saving the pivot candidates to pickle
    -Created a shell for greedy_pivot method that reads the pivot candidates from pickle

--- src/stats/greedy.py
+++ src/stats/greedy.py
@@ -1,4 +1,6 @@
+from ast import Raise
 import sys
+import pickle
 
 sys.path.insert(1, '../')
 import argparse
@@ -16,14 +18,23 @@ class Greedy:
         self.input_path = inputPath
         self.k = k
 
-    def greedy_pivot(self):
+    def generate_temporal_pivot_candidates(self):
+        # Opening the input file containing the addresses of the temporal graphs
         with open(self.input_path, "r") as f:
+            # Read the file row by row
             for row in f:
-                if not row.strip():  # check if line is blank
+                # check if line is blank and skip it in affirmative case
+                if not row.strip():  
                     continue
+
+                # Split the parameters in the row
                 row = row.split()
+
+                # Check that the row contains the right ammount of attributes
                 if len(row) > 3:
                     raise Exception('In input file, incorrect line: {}'.format(row))
+
+                # Read the filename of a temporal graph and its properties
                 g_path = row[0]
                 dummy_node = None
                 is_directed = True
@@ -31,12 +42,18 @@ class Greedy:
                     dummy_node = int(row[1])
                 if len(row) > 2:
                     is_directed = False
+                
+                # Creating the temporal graph
                 #graph_name = g_path.rsplit('/', 1)[1]
                 print("Computing pivots")
                 g = tg.Graph(file_path=self.input_path, is_directed=is_directed, latest_node=dummy_node)
+                
+                # Get the max and minimun time steps in the graph and the number of nodes
                 t_min,t_max = g.get_time_interval()
                 nv = g.get_num_nodes()
                 print("NODE NUMBER ",nv, "T_min = ",t_min,"  T_max = ",t_max)
+
+                # Create all temporal pivot candidates
                 pivots = {}
                 for node in range(0,nv):
                     if node in g.get_temporal_adj_list().keys():
@@ -46,6 +63,8 @@ class Greedy:
                                 "FW" :[]
                             }
                 print("PIVOT NUMBER ",len(pivots))
+
+                # Get the pairs reachable throught each temporal pivot candidate
                 for pivot in pivots.keys():
                     print("COMPUTING PIVOT = ",pivot)
                     a = ifub(g,[pivot])
@@ -55,8 +74,39 @@ class Greedy:
                     pivots[pivot]["FW"] = FW
                 print(pivots)
 
+                outfile = open(g_path + '_pivot_candidates.pickle','wb')
+                pickle.dump(pivots, outfile)
+                outfile.close()
 
+
+    def greedy_pivot(self):
+         # Opening the input file containing the addresses of the temporal graphs
+        with open(self.input_path, "r") as f:
+            # Read the file row by row
+            for row in f:
+                # check if line is blank and skip it in affirmative case
+                if not row.strip():  
+                    continue
+
+                # Split the parameters in the row
+                row = row.split()
+
+                # Check that the row contains the right ammount of attributes
+                if len(row) > 3:
+                    raise Exception('In input file, incorrect line: {}'.format(row))
+
+                # Read the filename of a temporal graph and its properties
+                g_path = row[0]
+
+                # Read the temporal pivot candidates for that graph
+                infile = open(g_path + '_pivot_candidates.pickle','rb')
+                p_candidates = pickle.load(infile)
+                infile.close()
+                
+        NotImplementedError()
+
+ 
 input ='/home/antoniocruciani/PycharmProjects/Link-stream-diameter/src/stats/graphs/SNAP/sx-mathoverflow-a2q.txt.sor'
 k = 3
 alg = Greedy(input,k)
-alg.greedy_pivot()
+alg.generate_temporal_pivot_candidates()
